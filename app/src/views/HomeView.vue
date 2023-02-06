de443e2cecc7d6cd51ae2d22d4dfd6cb6287df3a
Peter Roto <peterdroto@gmail.com>
2023-02-06 16:29:44 -0600

    init commit

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import DealCalculations from '@/components/DealCalculations.vue'; // @ is an alias to /src
+
+</script>
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <DealCalculations />
+  </div>
+</template>
